1a55941f3c61d503559f6bb6beaaaac25942f500
kevingunawanc14 <c14200005@john.petra.ac.id>
2022-07-11 16:33:47 +0700

    go

---
+++ create_account.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+
+  <title>Create Account</title>
+</head>
+<style>
+  .bgLogin {
+    background-image: url("https://images.unsplash.com/photo-1560253023-3ec5d502959f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+    background-repeat: no-repeat;
+    /* background-attachment: fixed; */
+    background-size: cover;
+    /* background-position: center; */
+    min-height: 200px;
+  }
+
+  .fa-fist-raised{
+    color: red;
+  }
+</style>
+
+<body>
+  <!-- <h1>Login</h1> -->
+
+  <div class="container-fluid">
+    <div class="row" style="min-height: 100vh;">
+      <div class="col-12 col-lg-4" style="background-color: #f9f9f9;">
+
+      
+        <div class="row">
+          <div class="col-6 col-lg-5">
+            <i class="fas fa-fist-raised mt-5 fa-4x" style="float: right;"></i>
+          </div>
+          <div class="col-6 col-lg-7">
+            <h3 class="mt-5" style="color: red; font-weight: bold;"> ABSENT </h3>
+            <h3  style="color: red; font-weight: bold;">POINT</h3>
+          </div>
+        </div>
+
+        <!-- <h1 class="mt-5" style="text-align: center;"><i class="fas fa-fist-raised"></i>
+          Absent </h1>
+        <h1 style="text-align: center;">Point</h1> -->
+
+
+
+
+        <h2 class="mt-5" style="text-align: center;">Create Account</h2>
+
+        <div class="form-floating mb-3 mx-5">
+          <input type="username" class="form-control" id="floatingInput" placeholder="USERNAME">
+          <label for="floatingInput" style="font-weight:bold;color: #828282;">USERNAME</label>
+        </div>
+        <div class="form-floating mx-5">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="PASSWORD">
+          <label for="floatingPassword" style="font-weight:bold; color: #828282;">PASSWORD</label>
+        </div>
+
+
+        <center><a href="index.php"><button type="button" class="btn btn-danger mb-5" style="margin-top: 45vh; width: 60px; height: 60px;border-radius: 17px;"><i class="fas fa-arrow-right"></i></button></center></a>
+
+        <a href="login.php" style="text-align: center; font-weight: bold; font-size: xx-small; color: black; text-decoration: none;"><p>SIGN IN</p></a>
+
+
+      </div>
+      <div class="col-12 col-lg-8 bgLogin">
+
+      </div>
+    </div>
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+require_once("include/navbar.php")
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
+
+
+
+
+  <title>Home</title>
+</head>
+
+<body>
+
+
+  <!-- rank -->
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <center><img src="assets/Iron_1_Rank.png" alt=""></center>
+        <h1 style="text-align: center; ">IRON 1</h1>
+        <div class="progress">
+          <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+      </div>
+
+    </div>
+    <div class="row">
+      <div class="col">
+        <p>RANK RATING <span style="float: right;">83/100</span></p>
+
+      </div>
+
+    </div>
+  </div>
+
+
+  <!-- tanggal -->
+
+  <div class="container">
+    <!-- <div class="row">
+      <h1>June</h1>
+    </div> -->
+    <div class="row">
+      <center>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-2">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Senin </h5>
+              <P>26-June-2022</P>
+              <p>Pagi</p>
+              <button type="button" class="btn btn-secondary">Absent avabile at 05.00-07.00</button>
+              <p>Siang</p>
+              <button type="button" class="btn btn-secondary">Absent avabile at 11.00-13.00</button>
+              <p>Malam</p>
+              <button type="button" class="btn btn-secondary">Absenxxxt avabile at 17.00-20.00</button>
+            </div>
+          </div>
+
+        </div>
+      </center>
+
+
+
+
+
+
+
+    </div>
+  </div>
+
+</body>
+
+
+<?php
+
+echo "marow <br>";
+date_default_timezone_set("Asia/Jakarta");
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "The time is " . date("H:i:s");
+
+echo "<br>";
+
+$pagi = date("H") - 07;
+// $malam =
+// $siang =
+
+echo $pagi;
+
+?>
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
