3b5b031fe7b57d628910f6771894d5ca921a9513
kevingunawanc14 <c14200005@john.petra.ac.id>
2022-07-14 09:51:37 +0700

    xx

--- career.php
+++ career.php
@@ -1,26 +1,58 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Career History</h1>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+  <!-- Font Awesome -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+  <title>Hello, world!</title>
+</head>
 
-
-
-
-
+<body>
+  <h1>Career History</h1>
 
 
+  <div class="container">
+    <div class="row">
+
+      <div class="col-12">
+        <div class="card text-center">
+          <div class="card-header">
+            <center><i class="fa-solid fa-cow"></i></center>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title" style="color: #5cb4a4;">+12</h5>
+          </div>
+          <div class="card-footer text-muted">
+            13-JUL-22 9:08 AM
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 mt-1">
+        <div class="card text-center">
+          <div class="card-header">
+            <center><i class="fa-solid fa-cow"></i></center>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title" style="color: #5cb4a4;">+11</h5>
+          </div>
+          <div class="card-footer text-muted">
+            13-JUL-22 9:08 AM
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
 
 
 
@@ -29,6 +61,16 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
+
+
+
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
 </html>
