08f55dc2d7a7c50456636158af51cf2102ca538d
albert1911 <albertvalentino19@gmail.com>
2022-07-20 14:22:57 +0700

    absent backend

---
+++ add_points.php
@@ -0,0 +1,60 @@
+<?php
+    include "include/connect.php";
+
+    $username = $_SESSION["username"];
+    $tanggal = date("Y/m/d");
+    $waktu = date("H:i:s");
+    $rating = $_GET["poin"];
+
+    $stmt_user = $pdo->query("SELECT * FROM user WHERE nama = '$username'");
+    $id_user = -1;
+    if ($row = $stmt_user->fetch()) {
+        if ($row["nama"] == $username) {
+            $id_user = $row["id"];
+        }
+    }
+
+    // echo date("His");
+
+    // status pagi, siang, malam
+    $pagi = false; $siang = false; $malam = false;
+    if (date("His") >= 50000 && date("His") <= 70000) {
+        $pagi = true;
+        echo "pagi";
+    } else if (date("His") >= 110000 && date("His") <= 130000) {
+        $siang = true;
+        echo "siang";
+    } else if (date("His") >= 170000 && date("His") <= 190000) {
+        $malam = true;
+        echo "malam";
+    } else {
+        echo ('<script> location.replace("index.php?stat=1"); </script>');
+    }
+
+    $stmt_history = $pdo->query("SELECT * FROM history WHERE id_user = $id_user");
+    while ($row = $stmt_history->fetch()) {
+        // change time format from hh:mm:ss -> hhmmss
+        $new_time = date("His", strtotime($row["waktu"]));
+        if ($pagi && $new_time >= 50000 && $new_time <= 70000) {
+            echo ('<script> location.replace("index.php?stat=2"); </script>');
+        } else if ($siang && $new_time >= 110000 && $new_time <= 130000) {
+            echo ('<script> location.replace("index.php?stat=2"); </script>');
+        } else if ($malam && $new_time >= 170000 && $new_time <= 190000) {
+            echo ('<script> location.replace("index.php?stat=2"); </script>');
+        }
+    }
+
+    if ($id_user != -1) {
+        // insert history
+        $sql_insert = "INSERT INTO `history`(`id`, `tanggal`, `waktu`, `rating`, `id_user`) VALUES (NULL,'$tanggal','$waktu',$rating,$id_user)";
+        $pdo->exec($sql_insert);
+
+        // update rating
+        $sql_update = "UPDATE `user` SET `rating`=(SELECT rating FROM user WHERE id = $id_user)+$rating WHERE id = $id_user";
+        $pdo->exec($sql_update);
+        
+        echo ('<script> location.replace("index.php?stat=0"); </script>');
+    } else {
+        echo "ERROR";
+    }
+?>
--- game.php
+++ game.php
@@ -1,11 +1,24 @@
 <?php
+  require_once("include/navbar.php");
+  require_once("include/connect.php");
 
-date_default_timezone_set("Asia/Jakarta");
+  date_default_timezone_set("Asia/Jakarta");
 
+  // jika belum log in
+  if (!isset($_SESSION["username"])) {
+    echo ('<script> location.replace("login.php"); </script>');;
+  }
 
-
-
-
+  if (isset($_GET["stat"])) {
+    
+    if ($_GET["stat"] == 0) { // jika berhasil absen
+      echo "SUCCESS"; 
+    } else if ($_GET["stat"] == 1) {
+      // jika waktu absen tidak sesuai
+    } else if ($_GET["stat"] == 2) {
+      // jika sudah pernah absen di waktu itu
+    }
+  }
 ?>
 
 <!doctype html>
@@ -22,75 +35,29 @@ date_default_timezone_set("Asia/Jakarta");
   <!-- Font Awesome -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-  <title>Game</title>
+  <title>Home</title>
 </head>
 
 <body>
-
-<!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"> <i class="fas fa-fist-raised fa-1x"></i></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="">ABSENT</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="">LEADERBOARD</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="">HOW TO PLAY</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="">SETTING</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="">LOGOUT</a>
-                    </li>
-
-
-                </ul>
-
-                <form class="d-flex">
-
-                    <button class="btn btn-outline-dark rounded-pill" type="submit">USERNAME</button>
-                </form>
-
-            </div>
-        </div>
-    </nav>
-
-  
-
   <!-- rank -->
   <div class="container mt-5">
     <div class="row">
       <div class="col">
-        <!-- <center><img src="assets/Iron_1_Rank.png" alt=""></center> -->
         <center><i class="fa-solid fa-cow"></i></center>
         <h3 style="text-align: center; ">COW 1</h3>
         <div class="progress">
           <div id="rating" data-rating="0" class="progress-bar bg-success" role="progressbar" style="width: 0%" aria-valuenow="O" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
       </div>
-
     </div>
     <div class="row">
       <div class="col">
         <p>RANK RATING <span style="float: right;" id="angkaRating">0</span></p>
-
       </div>
-
     </div>
   </div>
 
-
   <!-- tanggal -->
-
   <div class="container">
     <!-- <div class="row">
       <h1>June</h1>
@@ -98,21 +65,16 @@ date_default_timezone_set("Asia/Jakarta");
     <div class="row">
       <center>
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-2">
-
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">
                 <?php
-                $mydate = getdate(date("U"));
-                echo "$mydate[weekday]"
+                  $mydate = getdate(date("U"));
+                  echo "$mydate[weekday]";
                 ?>
               </h5>
 
-              <p>
-                <?php
-                echo date("d-M-Y");
-                ?>
-              </p>
+              <p><?php echo date("d-M-Y"); ?></p>
 
 
               <?php
@@ -134,13 +96,12 @@ date_default_timezone_set("Asia/Jakarta");
   
                   <button type='button' class='btn btn-danger mb-3' disabled >Absent avabile at 11.00 - 13.00</button>
                   
-  
                   <p>$malam</p>
                   
                   <button type='button' class='btn btn-danger mb-3'  disabled >Absent avabile at 17.00 - 19.00</button>
 
-       
-                  ";
+                ";
+
               } else if ($waktu_sekarang >= 11 && $waktu_sekarang <= 13) {
                 echo
 
@@ -197,18 +158,11 @@ date_default_timezone_set("Asia/Jakarta");
   
                   <p>$malam</p>
                   
-                  <button type='button' class='btn btn-danger mb-3'   disabled  >Absent avabile at 05.00 - 07.00</button>
+                  <button type='button' class='btn btn-danger mb-3'  onclick='absent()'  >Absent avabile at 05.00 - 07.00</button>
 
        
                   ";
-              }
-
-
-
-
-
-
-              ?>
+              } ?>
 
               <!-- <P>26-June-2022</P>
               <p>Pagi</p>
@@ -218,73 +172,51 @@ date_default_timezone_set("Asia/Jakarta");
               <p>Malam</p>
               <button type="button" class="btn btn-secondary" data-jam="17.00" onclick="absent(this.getAttribute('data-jam'))">Absent avabile at 17.00-20.00</button> -->
 
-
             </div>
           </div>
-
         </div>
       </center>
-
-
-
-
-
-
-
     </div>
   </div>
-
 </body>
 
 
 <!-- <?php
+  echo "marow <br>";
+  date_default_timezone_set("Asia/Jakarta");
+  echo "Today is " . date("Y/m/d") . "<br>";
+  echo "The time is " . date("H:i:s");
 
-      echo "marow <br>";
-      date_default_timezone_set("Asia/Jakarta");
-      echo "Today is " . date("Y/m/d") . "<br>";
-      echo "The time is " . date("H:i:s");
+  echo "<br>";
 
-      echo "<br>";
+  $pagi = date("H") - 07;
+  // $malam =
+  // $siang =
 
-      $pagi = date("H") - 07;
-      // $malam =
-      // $siang =
-
-      echo $pagi;
-
-      ?> -->
-
-
-
+  echo $pagi;
+?> -->
 
 <script>
-
   function absent() {
-    
     const data = document.querySelector('#rating');
-    
 
+    // get data attribute
     dataRating = parseInt(data.dataset.rating);
-    
-
+    // tambah data sesuai random-an
     dataRating += parseInt(Math.floor(Math.random() * 4) + 10);
-
+    // update angka progress bar
     document.getElementById("angkaRating").innerHTML = dataRating+"/100"
-
+    // update panjang progress bar
+    var temp = dataRating;
     dataRating = document.getElementById("rating").style.width = dataRating+"%";
+    // update data attribute
+    data.dataset.rating = dataRating;
 
-
-    
-
+    location.replace("add_points.php?poin=" + temp);
   }
-
-
 </script>
 
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
 
 </body>
 
