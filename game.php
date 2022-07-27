a176faa31ede2fe1934af92e6e6e863c55c65e24
kevingunawanc14 <c14200005@john.petra.ac.id>
2022-07-27 13:12:59 +0700

    1

--- game.php
+++ game.php
@@ -9,18 +9,33 @@ if (!isset($_SESSION["username"])) {
   echo ('<script> location.replace("login.php"); </script>');
 }
 
-if (isset($_GET["stat"])) {
+// if (isset($_GET["stat"])) {
 
-  if ($_GET["stat"] == 0) { // jika berhasil absen
-    echo "SUCCESS";
-  } else if ($_GET["stat"] == 1) {
-    // jika waktu absen tidak sesuai
-    echo "<script> alert('Waktu Absen Tidak Sesuai'); </script>";
-  } else if ($_GET["stat"] == 2) {
-    // jika sudah pernah absen di waktu itu
-    echo "<script> alert('Anda Sudah Melakukan Absen') </script>";
-  }
-}
+//   if ($_GET["stat"] == 0) { // jika berhasil absen
+//     echo "SUCCESS";
+//   } else if ($_GET["stat"] == 1) {
+//     // jika waktu absen tidak sesuai
+//     echo "<script> alert('Waktu Absen Tidak Sesuai'); </script>";
+//   } else if ($_GET["stat"] == 2) {
+//     // jika sudah pernah absen di waktu itu
+//     echo "<script> alert('Anda Sudah Melakukan Absen') </script>";
+//   }
+// }
+
+
+$sql = 'SELECT * FROM `user` WHERE `nama` = ?';
+
+$checksql = $pdo->prepare($sql);
+$checksql->execute([$_SESSION["username"]]);
+
+$row = $checksql->fetch();
+
+
+
+
+$rating = $row["rating"];
+
+
 ?>
 
 <!doctype html>
@@ -54,8 +69,81 @@ if (isset($_GET["stat"])) {
   <div class="container mt-5">
     <div class="row">
       <div class="col">
-        <center><i class="fa-solid fa-cow"></i></center>
-        <h3 style="text-align: center; ">COW 2</h3>
+        <center><?php 
+        
+        if ($row['rating'] <= 100) {
+          echo "
+
+          <i class='fa-solid fa-cow'></i>
+
+            ";
+        } else if ($row['rating'] >= 101 && $row['rating'] <= 200) {
+          echo "
+
+          <i class='fa-solid fa-dog'></i>
+          
+          ";
+        } else if ($row['rating'] >= 201 && $row['rating'] <= 300) {
+          echo "
+
+          <i class='fa-solid fa-hippo'></i>
+          
+          ";
+        } else if ($row['rating'] >= 301 && $row['rating'] <= 400) {
+          echo "
+
+          <i class='fa-solid fa-otter'></i>
+          
+          ";
+        } else if ($row['rating'] >= 401) {
+          echo "
+
+         <i class='fa-solid fa-dragon'></i>
+          
+          ";
+        }
+        
+        
+        
+        ?></center>
+        <h3 style="text-align: center; "><?php 
+        
+        
+        if ($row['rating'] <= 100) {
+          echo "
+
+            COW
+
+            ";
+        } else if ($row['rating'] >= 101 && $row['rating'] <= 200) {
+          echo "
+
+           DOG
+          
+          ";
+        } else if ($row['rating'] >= 201 && $row['rating'] <= 300) {
+          echo "
+
+           HIPPO
+          
+          ";
+        } else if ($row['rating'] >= 301 && $row['rating'] <= 400) {
+          echo "
+
+          OTTER
+          
+          ";
+        } else if ($row['rating'] >= 401) {
+          echo "
+
+         DRAGON
+          
+          ";
+        }
+        
+        
+        
+        ?></h3>
         <div class="progress">
           <div id="rating" data-rating="0" class="progress-bar bg-success" role="progressbar" style="width: 0%" aria-valuenow="O" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
@@ -63,7 +151,7 @@ if (isset($_GET["stat"])) {
     </div>
     <div class="row">
       <div class="col">
-        <p>RANK RATING <span style="float: right;" id="angkaRating">0</span></p>
+        <p>RANK RATING <span style="float: right;" id="angkaRating"><?php echo $rating  ?></span></p>
       </div>
     </div>
   </div>
@@ -216,6 +304,8 @@ if (isset($_GET["stat"])) {
 
 
   }
+
+  
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
